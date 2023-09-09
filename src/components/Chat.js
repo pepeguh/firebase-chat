4c7cce22e2a88270fde6a5e10f670e5e9c21967d
pepeguh <141973944+pepeguh@users.noreply.github.com>
2023-09-09 15:32:38 +1200

    major changes
    delete message, adaptive width of messages, semi-anti-spam system, check empty messages

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -3,7 +3,7 @@ import { Context } from '..';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import {useCollection} from 'react-firebase-hooks/firestore';
 import { Avatar, Container, Grid } from '@mui/material';
-import { collection, query, orderBy,addDoc  } from 'firebase/firestore';
+import { collection, query, orderBy,addDoc, deleteDoc, doc  } from 'firebase/firestore';
 import Loader from './Loader';
 import { serverTimestamp } from 'firebase/firestore';
 
@@ -17,29 +17,57 @@ const Chat = () => {
   const messagesCollection = collection(firestore, 'messages');
   const messagesQuery = query(messagesCollection, orderBy('createdAt'));
   const [messages, loading] = useCollection(messagesQuery);  
-
   const messagesContainerRef = useRef();//для автопрокрутки
 
+  const [canSendMessage, setCanSendMessage] = useState(true);
+  const screenWidth = window.innerWidth || document.documentElement.clientWidth;
+  const root = document.documentElement;
+if (screenWidth < 768) { // Пример: изменить maxWidth для экранов меньше 768px
+  root.style.setProperty('--max-width', '230px');
+} else if (screenWidth < 1024) { // Пример: изменить maxWidth для экранов меньше 1024px
+  root.style.setProperty('--max-width', '600px');
+} else {
+  root.style.setProperty('--max-width', '750px'); // Значение по умолчанию для больших экранов
+}
 
   const sendMessage = async () => {
+    if (!canSendMessage) {
+      return console.log('Подождите, перед отправкой нового сообщения должно пройти хотя бы 2 секунды.');
+    }
+    if (value.trim() === ''){
+      return console.log('пустое сообщение')
+    }
     try {
       const docRef = await addDoc(messagesCollection, {
         uid: user.uid,
         displayName: user.displayName,
         photoURL: user.photoURL,
-        text: value,
+        text: value.trim(),
         createdAt: serverTimestamp(),
       });
       console.log('Document written with ID: ', docRef.id);
       setValue('');
+
     } catch (error) {
       console.error('Error adding document: ', error);
     }
-  
-    setValue('')
+    setCanSendMessage(false); // Устанавливаем состояние в false перед отправкой
 
+    setTimeout(() => {
+      setCanSendMessage(true); // После истечения таймера устанавливаем состояние в true
+    }, 2000);
+  
+    setValue('');
   };
 
+  const deleteMessage = async (messageUid)=>{
+    try{
+      const messageRef = doc(messagesCollection, messageUid); 
+      await deleteDoc(messageRef);
+      console.log(`Документ успешно удален ${messageUid}`);
+    }catch (error){
+      console.error('Error deleting document: ', error)}
+  }
 
   const messagesArray = messages ? messages.docs.map((doc) => ({
     id: doc.id,
@@ -86,15 +114,29 @@ const Chat = () => {
               <Grid  container style={{justifyContent:user.uid === message.uid ? 'flex-end': 'flex-start'}}>
                 
                 <Avatar style={{marginTop:'2px'}} src={message.photoURL}/>
-                  <div style={{marginLeft:'10px',marginTop:'14px'}}>{message.displayName }{ user.uid === message.uid ? ' (Вы)':''}</div>
-              
+                  <div style={{marginLeft:'10px',marginTop:'14px',}}>{message.displayName }</div>
+                  {user.uid === message.uid ?  <button
+                   style={{
+                    border:'none',
+                    background:'transparent',
+                    fontSize:'20px',
+                    marginLeft:'2px',
+                    opacity: 0, /* Устанавливаем начальное значение непрозрачности */
+                    transition: 'opacity 0.2s', /* Добавляем плавный переход */
+                 
+                   }}
+                   onMouseEnter={(e) => e.target.style.opacity = 1} /* Показываем кнопку при наведении */
+                   onMouseLeave={(e) => e.target.style.opacity = 0} /* Скрываем кнопку при уходе курсора */
+                   onClick={()=> deleteMessage(message.id)}
+                    > &#128465;</button>:'' }
+                  
                 
                   </Grid>
-              <div style={{
+              <div className='message' style={{
                marginLeft:'5px',
                marginTop:'4px',
                textAlign: user.uid === message.uid ? 'right' : 'left',
-               maxWidth:'600px',
+              
                whiteSpace: 'pre-wrap',
                wordWrap: 'break-word',
                  }}>{message.text}</div>
@@ -126,7 +168,7 @@ const Chat = () => {
                  borderRadius: '5px',
                  backgroundColor:'transparent',
                  borderColor:'#5d3954',
-                              
+                 paddingLeft:'5px'        
                 }}
                 />
                 
